28909c85f9a24e5c723b3e9e44eb074252c5b0c9
Kanezal <58330966+Kanezal@users.noreply.github.com>
2024-01-14 13:56:12 +0000

    фикс 401 от верселя

--- api/interactions.js
+++ api/interactions.js
@@ -3,26 +3,30 @@ const { publicKey } = require("../config.json")
 const { isGuard } = require("../api/functions/checks")
 const router = require('express').Router()
 
+router.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  next();
+});
+
 const commands = {}
 const normalizedPath = require("path").join(__dirname, "commands");
 require("fs").readdirSync(normalizedPath).forEach(function (file) {
-    commands[file.slice(0, -3)] = require("./commands/" + file)[file.slice(0, -3)]
+   commands[file.slice(0, -3)] = require("./commands/" + file)[file.slice(0, -3)]
 });
 
 /**
  * POST reports
- * 
+ *
  * @return message to user
  */
 router.post('/', verifyKeyMiddleware(publicKey), async (req, res) => {
-    const message = req.body;
-    if (message.type === InteractionType.APPLICATION_COMMAND) {
-        if (!isGuard)
-            return undefined
+   const message = req.body;
+   if (message.type === InteractionType.APPLICATION_COMMAND) {
+       if (!isGuard)
+           return undefined
 
-        await commands[message.data.name](res, message)
-    }
+       await commands[message.data.name](res, message)
+   }
 });
-
 
 module.exports = router;
--- index.js
+++ index.js
@@ -3,8 +3,13 @@ const express = require('express')
 const app = express()
 const PORT = process.env.PORT || 3000
 
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  next();
+});
+
 app.use("/api/interactions", require("./api/interactions"))
 
 app.listen(PORT, () => {
-    console.log(`Example app listening on port ${PORT}`)
+   console.log(`Example app listening on port ${PORT}`)
 })
